b4cf1a77f11c8d42b1055e056b8b735d93f3878d
Anya <anyasanders1@gmail.com>
2023-05-16 19:59:52 -0500

    loading icon and db addition

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,18 @@
 import FeedbackItem from "./FeedbackItem"
 import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from "react"
+import Spinner from "./shared/Spinner"
 import FeedbackContext from "../context/FeedbackContext"
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No feedback yet</p>
     }
-  return (
-    <div className="feedback-list">
+
+    return isLoading ? (<Spinner />) : (
+        <div className="feedback-list">
         <AnimatePresence>
         {feedback.map((item) => (
             <motion.div
@@ -24,7 +26,7 @@ function FeedbackList() {
     ))}
         </AnimatePresence>
         </div>
-  )
+    )
 }
 
 export default FeedbackList
